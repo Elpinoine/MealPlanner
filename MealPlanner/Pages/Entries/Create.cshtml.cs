d75b48db478d86de196f769500bf58da21209197
Elpinoine <elpinoine@mac.com>
2020-07-11 22:12:41 +0200

    Hidden planid when creating new Entry

--- MealPlanner/Pages/Entries/Create.cshtml.cs
+++ MealPlanner/Pages/Entries/Create.cshtml.cs
@@ -9,36 +9,38 @@ using MealPlanner.Models;
 
 namespace MealPlanner.Pages.Entries
 {
-    public class CreateModel : PageModel
+  public class CreateModel : PageModel
+  {
+    private readonly MealPlanner.Models.MealPlannerContext _context;
+
+    public CreateModel(MealPlanner.Models.MealPlannerContext context)
     {
-        private readonly MealPlanner.Models.MealPlannerContext _context;
-
-        public CreateModel(MealPlanner.Models.MealPlannerContext context)
-        {
-            _context = context;
-        }
-
-        public IActionResult OnGet()
-        {
-        ViewData["MealId"] = new SelectList(_context.Meal, "ID", "ID");
-        ViewData["PlanId"] = new SelectList(_context.Plan, "ID", "ID");
-            return Page();
-        }
-
-        [BindProperty]
-        public PlanEntry PlanEntry { get; set; }
-
-        public async Task<IActionResult> OnPostAsync()
-        {
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
-
-            _context.PlanEntry.Add(PlanEntry);
-            await _context.SaveChangesAsync();
-
-            return RedirectToPage("./Index");
-        }
+      _context = context;
     }
+
+    public IActionResult OnGet(int planid)
+    {
+
+      ViewData["MealId"] = new SelectList(_context.Meal, "ID", "ID");
+      //ViewData["PlanId"] = new SelectList(_context.Plan, "ID", "ID");
+      return Page();
+    }
+
+    [BindProperty]
+    public PlanEntry PlanEntry { get; set; }
+
+    public async Task<IActionResult> OnPostAsync(int planid)
+    {
+      if (!ModelState.IsValid)
+      {
+        return Page();
+      }
+
+      PlanEntry.PlanId = planid;
+      _context.PlanEntry.Add(PlanEntry);
+      await _context.SaveChangesAsync();
+
+      return RedirectToPage("./Index");
+    }
+  }
 }
